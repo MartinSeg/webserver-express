5ee12162ea8f75113ea29f2f605669cec6fc00aa
Martin Seghezzo <martinseghezzo@gmail.com>
2020-06-11 19:24:25 -0300

    Primer Commit

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -7,7 +7,6 @@ hbs.registerHelper("getAnio", () => {
 
 hbs.registerHelper("capitalizar", (texto) => {
   let palabras = texto.split(" ");
-  console.log(palabras);
   palabras.forEach((palabra, index) => {
     palabras[index] =
       palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", {
+    place: 'Ushuaia'
+  });
 });
 
 app.listen(port, () => {
